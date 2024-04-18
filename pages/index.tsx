cbae060552fb71b702e76cb82bfa2ef1c96d9188
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-04-18 04:03:25 +0200

    :art: working on  the navbar section  :art:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
+import MobileNav from '@/Components/Helper/MobileNav';
 import Navbar from '@/Components/Helper/Navbar';
 import React from 'react'
 
 const HomePage = () => {
+
+  
+
   return (
     <div className='overflow-x-hidden'>
       {/* NavBar */}
+      <MobileNav/>
       <Navbar/>
     </div>
   )
