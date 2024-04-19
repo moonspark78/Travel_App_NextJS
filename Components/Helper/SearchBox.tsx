afbb832fa96ecc43581e0f99e6eb58d853efe352
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-04-19 03:59:30 +0200

    :tada: still  working on the hero section  :tada:

--- Components/Helper/SearchBox.tsx
+++ Components/Helper/SearchBox.tsx
@@ -1,12 +1,14 @@
-import { MapIcon } from '@heroicons/react/16/solid'
+import { CalendarDaysIcon, MapIcon } from '@heroicons/react/16/solid'
+import { MapPinIcon } from '@heroicons/react/20/solid'
 import React from 'react'
 
 const SearchBox = () => {
   return (
-    <div className='bg-white rounded-lg p-8 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 items-center justify-center gap-[2rem]
+    <div className='bg-white rounded-lg p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 items-center justify-center gap-[1.6rem]
     mt-[3rem] w-[80%]'>
-      <div className='flex items-center space-x-6'>
-      <MapIcon className='w-[1.5rem] h-[1.5rem] text-red-600'/>
+      {/* First search Input */}
+      <div className='flex items-center space-x-2'>
+        <MapIcon className='w-[1.4rem] h-[1.4rem] text-red-600'/>
         <div>
           <p className='text-[18px] font-semibold mb-[0.2rem]'>Location</p>
           <input
@@ -14,6 +16,38 @@ const SearchBox = () => {
             type="text" 
             placeholder='Where are you going?'
           />
+        </div>
+      </div>
+      {/* Second search Input */}
+      <div className='flex items-center space-x-5'>
+        <CalendarDaysIcon className='w-[1.5rem] h-[1.5rem] text-red-600'/>
+        <div>
+          <p className='text-[18px] font-semibold mb-[0.2rem]'>Start Date</p>
+          <input
+            className='outline-none border-none'
+            type="date" 
+            placeholder='Where are you going?'
+          />
+        </div>
+      </div>
+      {/* Third search Input */}
+      <div className='flex items-center space-x-6'>
+        <CalendarDaysIcon className='w-[1.5rem] h-[1.5rem] text-red-600'/>
+        <div>
+          <p className='text-[18px] font-semibold mb-[0.2rem]'>End Date</p>
+          <input
+            className='outline-none border-none'
+            type="date" 
+            placeholder='Where are you going?'
+          />
+        </div>
+      </div>
+      {/* Fourth search Input */}
+      <div className='flex items-center space-x-6'>
+        <CalendarDaysIcon className='w-[1.5rem] h-[1.5rem] text-red-600'/>
+        <div>
+          <p className='text-[18px] font-semibold mb-[0.2rem]'>End Date</p>
+          <p className="text-[18px] font-normal">1 Guest 1 Room</p>
         </div>
       </div>
     </div>
