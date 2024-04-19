64b5d5b8619d8b12a5fe618676aac94caec399e0
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-04-19 03:32:30 +0200

    :art: still  working on the hero section  :art:

--- Components/Helper/SearchBox.tsx
+++ Components/Helper/SearchBox.tsx
@@ -1,8 +1,22 @@
+import { MapIcon } from '@heroicons/react/16/solid'
 import React from 'react'
 
 const SearchBox = () => {
   return (
-    <div>SearchBox</div>
+    <div className='bg-white rounded-lg p-8 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 items-center justify-center gap-[2rem]
+    mt-[3rem] w-[80%]'>
+      <div className='flex items-center space-x-6'>
+      <MapIcon className='w-[1.5rem] h-[1.5rem] text-red-600'/>
+        <div>
+          <p className='text-[18px] font-semibold mb-[0.2rem]'>Location</p>
+          <input
+            className='outline-none border-none'
+            type="text" 
+            placeholder='Where are you going?'
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
