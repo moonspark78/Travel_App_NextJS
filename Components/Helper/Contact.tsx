9ac77a9774449ce0a97db6c455a9b612491914c3
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-04-20 04:13:23 +0200

    :tada: working on the Contact section  :tada:

--- Components/Helper/Contact.tsx
+++ Components/Helper/Contact.tsx
@@ -6,6 +6,7 @@ const Contact = () => {
     <div className='pt-[8rem] pb-[3rem]'>
         <h1 className='heading'>Contact</h1>
         <div className='w-[80%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-[3rem] pt-[1.3rem] '>
+          <div>
             <Image 
                 src="/images/c1.png"
                 alt="fly"
@@ -13,6 +14,13 @@ const Contact = () => {
                 height={340}
                 className='object-contain mx-auto p-[1rem]'
             />
+          </div>
+          <div className='flex items-center justify-center flex-col'>
+            <h1 className='text-center w-[90%] lg:w-[70%] tracking-[0.1rem] mx-auto font-bold text-[20px] md:text-[30px] text-black opacity-70'>
+              Get special offers and more from Traveler
+            </h1>
+            <p>Subscribe to see the secret deals prices drop the moment you sign up</p>
+          </div>
         </div>
     </div>
   )
